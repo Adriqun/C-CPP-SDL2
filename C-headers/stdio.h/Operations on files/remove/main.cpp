ed77d9f5ed82c55b133e7e3ffb2120fea72dac6c
devmichalek <devmichalek@gmail.com>
2016-07-21 14:51:46 +0200

    remove file

---
+++ C-headers/stdio.h/Operations on files/remove/main.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main ()
+{
+    if( remove( "myfile.txt" ) != 0 )
+        perror( "Error deleting file" );
+    else
+        puts( "File successfully deleted" );
+
+    return 0;
+}
