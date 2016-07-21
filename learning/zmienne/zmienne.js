0069f63c82b1a568c10be7b3288b433aeb92a3bd
Adrian Michałek <adrmic98@gmail.com>
2016-07-21 15:26:02 +0200

    first meeting with javaScript

---
+++ learning/zmienne/zmienne.js
@@ -0,0 +1,10 @@
+<script>
+	var tekst = 'Przykładowy tekst';
+	var liczba = 230;
+	var max = Number.MAX_VALUE;
+
+	console.log( 9 + liczba + 4 );
+	console.log( 9 + max + 4 );
+	console.log( tekst );
+	
+</script>
