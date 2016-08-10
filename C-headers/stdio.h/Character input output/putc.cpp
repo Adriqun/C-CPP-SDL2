5c10189672bc6caf88846e5b0b141453e75c67c1
devmichalek <devmichalek@gmail.com>
2016-08-10 10:35:19 +0200

    part3

---
+++ C-headers/stdio.h/Character input output/putc.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main ()
+{
+  FILE * pFile;
+  char c;
+
+  pFile=fopen("alphabet.txt","wt");
+  for (c = 'A' ; c <= 'Z' ; c++) {
+    putc (c , pFile);
+    }
+  fclose (pFile);
+  return 0;
+}
