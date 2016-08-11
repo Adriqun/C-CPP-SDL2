fa32d70206678891fa0ce29b1c0f8e2cce4731c1
devmichalek <devmichalek@gmail.com>
2016-08-11 08:30:18 +0200

    I think, profit will be also expense

--- Investing CPP, SDL2/profit.cpp
+++ Investing CPP, SDL2/profit.cpp
@@ -1,1 +1,43 @@
 #include "profit.h"
+
+void Profit::sortString()
+{
+    for( unsigned i = 0; i < inputText.length(); i ++ )
+    {
+        if( inputText[ i ] == ' ' )
+        {
+            inputText.erase( i, 1 );
+        }
+    }
+
+    string newInputText = "";
+    int c = inputText.length()-1;
+    for( unsigned i = 0; i < inputText.length(); i ++ )
+    {
+        newInputText += inputText[ i ];
+
+        if( c%3 == 0 )
+        {
+            newInputText += ' ';
+        }
+
+        c --;
+    }
+    inputText = newInputText;
+}
+
+// Only +numbers
+long long Profit::strToInt( string s )
+{
+    long long tmp = 0;
+
+    for( unsigned i = 0; i < s.size(); i++ )
+    {
+        if( s[ i ] == ' ' )
+            continue;
+
+        tmp = 10 * tmp + s[ i ] - 48;
+    }
+
+    return tmp;
+}
--- Investing CPP, SDL2/profit.h
+++ Investing CPP, SDL2/profit.h
@@ -4,5 +4,22 @@
 
 class Profit
 {
-
+	Font font;
+	SDL_Color color;
+	
+	Texture texture;
+	string name;
+	
+	bool renderText;
+    string inputText;
+	
+	long long cost;
+	
+	bool focus;
+	
+public:
+	
+	void sortString();
+	long long strToInt( string s );
+	
 };
