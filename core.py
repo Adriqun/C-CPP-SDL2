ea134314e278bf7e98de78f4b836259a88fd3267
devmichalek <devmichalek@gmail.com>
2016-08-23 16:01:58 +0200

    core class contain e.g. screen width

---
+++ core.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+
+import pygame
+
+class Core:
+
+	def __init__( self, FPS, width, height, caption ):
+	
+		self.quit = False	#Main bool.
+		self.FPS = FPS		#Frames per second.
+
+		self.width = width	#Screen width
+		self.height = height	#Screen height
+		
+		pygame.init()				#INIT PYGAME
+		self.clock = pygame.time.Clock()	#Timer
+
+		self.window = pygame.display.set_mode( ( width, height ) )	#Window
+		pygame.display.set_caption( caption )				#Caption
+		
+
+	def getW( self ):
+		return self.width
+
+	def getH( self ):
+		return self.height
+
+	def getFPS( self ):
+		return self.FPS
+
+	def isQuit( self ):
+		return self.quit
+
+	def setQuit( self ):
+		self.quit = True
+
+	def fillDisplay( self ):
+		self.window.fill( [ 0xFF, 0xFF, 0xFF ] )
+
+	def tickClock( self ):
+		self.clock.tick( self.FPS )
+
+	def getWindow( self ):
+		return self.window
