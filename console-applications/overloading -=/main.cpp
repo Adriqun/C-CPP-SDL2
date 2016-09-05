82cb5ec2fb275dd0f04a49b51daf291aa03ca14e
devmichalek <devmichalek@gmail.com>
2016-09-05 21:04:31 +0200

    -=

---
+++ console-applications/overloading -=/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+class Square
+{
+	int a;
+	int area;
+	
+public:
+	
+	Square( int a )
+	{
+		this->a = a;
+		area = a*a;
+	}
+	
+	int operator -=( int area )
+	{
+		return this->area -= area;
+	}
+	
+	int getArea()
+	{
+		return area;
+	}
+	
+	ostream& operator <<( ostream& s )
+	{
+		return s << "Area: " << area << endl;
+	}
+	
+};
+
+int main(int argc, char **argv)
+{
+    Square square( 5 );
+	
+	square << cout;
+	
+	square -= 5;
+	
+	square << cout;
+	
+    return 0;
+}
