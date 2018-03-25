3d467669d28df801e1bffaeb8ab66d66ed91acab
devmichalek <devmichalek@gmail.com>
2018-03-25 13:17:15 +0200

    now with pure understanding

--- Advanced C++/const/main.cpp
+++ Advanced C++/const/main.cpp
@@ -38,3 +38,37 @@ int main()
 	
 	return 0;
 }
+
+/*
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int i = 0;
+	const int& ref = i;
+	const int* ptr = &i;
+
+	const_cast<int&>(ref) = 3;
+	*const_cast<int*>(ptr) = 3;
+	
+	cout << ref << ' ' << *ptr;
+	
+	// data is const
+	const int b = 0;
+	const int* c = NULL;
+	int const* d = NULL;
+	
+	// pointer is const
+	int* const e = NULL;
+	
+	// data and pointer are const
+	const int* const f = NULL;
+	
+	// const on the left of the * -> data is const
+	// const on the right of the * -> pointer is const
+	
+	return 0;
+}
+*/
