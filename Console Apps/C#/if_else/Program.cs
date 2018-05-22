82aa476b2c80743924f02f03bf09bafb5ebf1608
devmichalek <devmichalek@gmail.com>
2018-05-22 11:31:20 +0200

    some c# projects from 2016

---
+++ Console Apps/C#/if_else/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace if_else
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			int zmienna;
+			zmienna = Convert.ToInt32 (Console.ReadLine ());
+			if (zmienna < 100)
+				Console.WriteLine ("Wpisales liczbe mniejsze od 100");
+			else if(zmienna == 100)
+				Console.WriteLine ("Wpisales liczbe 100");
+			else
+				Console.WriteLine ("Wpisales liczbe wieksza od 100");
+		}
+	}
+}
---
+++ Console Apps/C#/pierwszy program/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Pierwszy_program
+{
+	class MainClass
+	{
+		void wczytam()
+		{
+			
+		}
+		
+		public static void Main ( string[] args )
+		{
+			var zmienna = "2016.02.08";
+			Console.Write ( zmienna );
+			Console.ReadKey ();
+		}
+	}
+}
