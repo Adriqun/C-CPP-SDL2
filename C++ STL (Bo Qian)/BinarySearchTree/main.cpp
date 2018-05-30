550575fe9959dc4b2dd40b85434395ad2b26f1a7
devmichalek <devmichalek@gmail.com>
2018-05-30 13:49:49 +0200

    main shows how BST works

---
+++ C++ STL (Bo Qian)/BinarySearchTree/main.cpp
@@ -0,0 +1,42 @@
+#include <cstdlib>
+#include <iostream>
+#include "BST.h"
+
+int main()
+{
+	int treeKeys[16] = { 50, 76, 21, 4, 32, 64, 15, 52, 14, 100, 83, 2,  3,  70, 87, 80 };
+	BST* tree = new BST;
+
+	tree->print();
+
+	for (int i = 0; i < 16; ++i)
+	{
+		tree->add(treeKeys[i]);
+	}
+
+	tree->remove(2);
+	// tree->printChildren(50);
+	tree->print();
+	std::cout << std::endl  << "Smallest: " << *tree->getSmallest() << std::endl;
+	std::cout << "Greatest: " << *tree->getGreatest() << std::endl;
+
+	std::cout << tree->size();
+
+	delete tree;
+	tree = nullptr;
+
+	/*for (int j = 0; j < 100; ++j)
+	{
+		tree = new BST;
+		for (int i = 0; i < 16; ++i)
+		{
+			tree->add(treeKeys[i]);
+		}
+		tree->print();
+
+		delete tree;
+		tree = nullptr;
+	}*/
+
+	return 0;
+}
