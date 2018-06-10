388f3faa2620c9fa319321ca9becef4ae7d528e6
devmichalek <devmichalek@gmail.com>
2018-06-10 10:06:36 +0200

    how to use comments, @ sign, @ECHO OFF

---
+++ Batch Scripts/Steve Jansen/comments, @ sign.bat
@@ -0,0 +1,10 @@
+REM This is a comment!
+:: This is a comment too (usually)!
+
+REM Turns off echo's
+REM Once we set ECHO’ing to off, we won’t need the @ operator again in our script commands.
+@ECHO OFF
+
+REM Turns on echo's
+ECHO ON
+
