bc58f79e3c90d2c3e47910709dc10500b9778c50
devmichalek <devmichalek@gmail.com>
2018-12-15 23:32:07 +0100

    Tcl Special Variables

---
+++ tcl, tk/special_variables.tcl
@@ -0,0 +1,10 @@
+#tcl has special variables built in
+#examples below
+
+puts "Tcl Version: $tcl_version"
+puts "Environment: $env(PATH)"
+#puts "Package Path: $tcl_pkgPath"
+puts "Library: $tcl_library"
+puts "Patch Level: $tcl_patchLevel"
+puts "Precision: $tcl_precision"
+puts "Startup File: $tcl_rcFileName"
