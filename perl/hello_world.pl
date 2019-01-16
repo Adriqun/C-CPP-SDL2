fb8bc8792d30d27e4332e9c31aff21cb68f1a868
devmichalek <devmichalek@gmail.com>
2019-01-16 22:55:34 +0100

    hello world

---
+++ perl/hello_world.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl
+
+# single line comment
+=begin
+this is
+multi
+line
+=cut
+
+print("Hello World\n");
+print('Hello World\n'); print("\n");
+print "Hello
+	   World\n";
+
+
+
+$a = 10;
+$var = <<"EOF";
+This is the syntax for here document and it will continue
+until it encounters a EOF in the first line.
+This is case of double quote so variable value will be 
+interpolated. For example value of a = $a
+EOF
+print "$var\n";
+
+$var = <<'EOF';
+This is case of single quote so variable value will be 
+interpolated. For example value of a = $a
+EOF
+print "$var\n";
