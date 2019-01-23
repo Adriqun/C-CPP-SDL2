0029e22819e5a3f41d91feda6bab9c5e301ad670
devmichalek <devmichalek@gmail.com>
2019-01-23 23:07:34 +0100

    theory of vstrings + literals like __LINE__

---
+++ perl/vstrings_literals.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+
+$smile  = v9786;
+$foo    = v102.111.111;
+$martin = v77.97.114.116.105.110; 
+
+print "smile = $smile\n";
+print "foo = $foo\n";
+print "martin = $martin\n";
+
+print "File name ". __FILE__ . "\n";
+print "Line Number " . __LINE__ ."\n";
+print "Package " . __PACKAGE__ ."\n";
+
+# they can not be interpolated
+print "__FILE__ __LINE__ __PACKAGE__\n";
