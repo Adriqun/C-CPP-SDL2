47c55b3f318458dbbcd209b7f257e2d9860d0112
devmichalek <devmichalek@gmail.com>
2019-05-06 09:38:42 +0200

    std_logic_vector, shifting

---
+++ vhdl/VHDLWHIZ/PART II/Tutorial 11, std_logic_vector and shift registers/main.vhd
@@ -0,0 +1,25 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity Comp is
+end entity;
+
+architecture Arch of Comp is
+	signal slv1 : std_logic_vector(7 downto 0);
+	signal slv2 : std_logic_vector(7 downto 0) := (others => '0');
+	signal slv3 : std_logic_vector(7 downto 0) := (others => '1');
+	signal slv4 : std_logic_vector(7 downto 0) := x"AA";
+	signal slv5 : std_logic_vector(0 to 7) := "10101010";
+	signal slv6 : std_logic_vector(7 downto 0) := "00000001";
+
+begin
+	process is
+	begin
+		wait for 10 ns;
+		for i in (slv6'left) downto (slv6'right + 1) loop
+			slv6(i) <= slv6(i-1);
+		end loop;
+		slv6(slv6'right) <= slv6(slv6'left);
+
+	end process;
+end architecture;
