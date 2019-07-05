fa0512cb0e47be9031a12272f8a97bc889668c54
devmichalek <devmichalek@gmail.com>
2019-07-05 23:50:40 +0200

    Add question 0.1

--- C++/Tests/answerslibs.cpp
+++ C++/Tests/answerslibs.cpp
@@ -2,6 +2,21 @@
 // email: devmichalek@gmail.com
 
 0.0 // The proper order for the specified needs is:
-    // Compile test.c - gcc -c test.c
-    // Create static library - ar -cvq libtest.a test.o
-    // Compile and create executable - gcc -o main main.c libtest.a
+    // Compile test.c:
+    //    gcc -c test.c
+    // Create static library:
+    //    ar -cvq libtest.a test.o
+    // Compile and create executable:
+    //    gcc -o main main.c libtest.a # or
+    //    gcc -o main main -L/path/to/current/directory -ltest
+
+0.1 // The proper order for the specified needs is:
+    // Compile test.c with -fPIC (Platform Independent Code flag)
+    //    gcc -fPIC -c test.c
+    // Create dynamic library
+    //    gcc -shared test.o -o libtest.so
+    // Compile and create executable:
+    //    gcc main.c -L/path/to/current/directory -ltest -o main
+    // Set $LD_LIBRARY_PATH variable:
+    //    LD_LIBRARY_PATH=path/to/current/directory
+    //    export LD_LIBRARY_PATH
--- C++/Tests/testlibs.cpp
+++ C++/Tests/testlibs.cpp
@@ -1,4 +1,6 @@
 // author: Adrian Michałek
 // email: devmichalek@gmail.com
 
-0.0 // Let's assume you have test.c with function func() and main.c which uses function func() from the static library libtest.a. Give the proper order of compilation, creation of executable binary and static library on linux. Write commands for the required needs.
+0.0 // Assume you have test.c with function func() and main.c which uses function func() from the static library libtest.a. Give the proper order of compilation, creation of executable binary and static library on linux. Write commands for the required needs.
+
+0.1 // Do the same thing as in the previous task but now for the dynamic library.
