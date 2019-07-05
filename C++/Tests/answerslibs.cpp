3174cfd53b78386a7fb511d46301afc7f73b4a16
devmichalek <devmichalek@gmail.com>
2019-07-06 00:41:00 +0200

    Add question 0.2

--- C++/Tests/answerslibs.cpp
+++ C++/Tests/answerslibs.cpp
@@ -20,3 +20,19 @@
     // Set $LD_LIBRARY_PATH variable:
     //    LD_LIBRARY_PATH=path/to/current/directory
     //    export LD_LIBRARY_PATH
+
+0.2 // You have to use --whole-archive and --no-whole-archive flag.
+    // These flags are linker flags so the use is actually -Wl,<flag>
+    // While creating shared library we specify --whole-archive that means take all the objects from static library even these not needed (add2() function is not used in dynamic library but it may be used by dynamic library users). Then turn off this flag by setting --no-whole-archive, this step is crucial to escape infinite loop error.
+
+    // Compile part.c and create static library:
+    //    gcc -c part.c
+    //    ar -cvq libpart.a part.o
+    // Compile test.c and create a shared library:
+    //    gcc -fPIC -c test.c
+    //    gcc -shared -Wl,--whole-archive libpart.a Wl,--no-whole-archive test.o -o libtest.so
+    // Set $LD_LIBRARY_PATH variable:
+    //    LD_LIBRARY_PATH=path/to/current/directory
+    //    export LD_LIBRARY_PATH
+    // Compile and create executable:
+    //    gcc main.c libtest.so -o main
--- C++/Tests/testlibs.cpp
+++ C++/Tests/testlibs.cpp
@@ -4,3 +4,32 @@
 0.0 // Assume you have test.c with function func() and main.c which uses function func() from the static library libtest.a. Give the proper order of compilation, creation of executable binary and static library on linux. Write commands for the required needs.
 
 0.1 // Do the same thing as in the previous task but now for the dynamic library.
+
+0.2 // Assume you have the following files:
+// part.c:
+int add2(int a, int b) {
+  return a + b;
+}
+
+int add3(int a, int b, int c) {
+  return a + b + c;
+}
+
+//  test.c
+int func(int x) {
+  int add2(int, int);
+  return x + add2(1, 2);
+}
+
+// main.c
+#include <stdio.h>
+
+int main()
+{
+  int func(int);
+  int add3(int, int, int);
+  int ret = func(3) + add3(1, 2, 3);
+  printf("ret=%d\n", ret);
+  return ret;
+}
+//  How would you create an exectutable main which uses dynamic library libtest.so which includes static library libpart.a? Note that shared library does not know that function add2() will be used by the library users.
