fc658274f116219c72cd08e9013e215ebd6fed22
devmichalek <devmichalek@gmail.com>
2019-07-30 10:09:38 +0200

    Correct function name

--- C++/Avanced - C and C++ Compiling/Chapter 9 - Page 158-163/static/main.c
+++ C++/Avanced - C and C++ Compiling/Chapter 9 - Page 158-163/static/main.c
@@ -7,7 +7,7 @@ int main(int argc, char** argv)
   int ret = 0;
   ret += staticlibfirst_function(1);
   ret += staticlibsecond_function(2);
-  ret += staticlibduplicate_function(3);
+  ret += staticlib_duplicate_function(3);
   printf("ret = %d\n", ret);
   return ret;
 }
