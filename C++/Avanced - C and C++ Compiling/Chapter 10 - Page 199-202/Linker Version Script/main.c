343737301959d969b34a9374ab520afbfda039c8
devmichalek <devmichalek@gmail.com>
2019-08-02 10:41:20 +0200

    Final update on Linker Version Script Example

---
+++ C++/Avanced - C and C++ Compiling/Chapter 10 - Page 199-202/Linker Version Script/main.c
@@ -0,0 +1,8 @@
+#include "sh.h"
+
+int main()
+{
+	first_function(1);
+	second_function(2);
+	return 0;
+}
--- C++/Avanced - C and C++ Compiling/Chapter 10 - Page 199-202/Linker Version Script/runme.sh
+++ C++/Avanced - C and C++ Compiling/Chapter 10 - Page 199-202/Linker Version Script/runme.sh
@@ -1,9 +1,18 @@
 gcc -Wall -g -O0 -fPIC -c sh.c
 gcc -g -shared sh.o -o libsh.so -Wl,--version-script,script
 readelf -S libsh.so
-echo ""
-echo ""
 readelf -V libsh.so
-echo ""
-echo ""
 readelf --symbols libsh.so | grep function
+
+# now compile, create main
+gcc -g -O0 -c main.c
+gcc main.o -Wl,-R. -Wl,-L. -Wl,-lsh -o main
+
+# run main
+./main
+
+# now if version of libsh.so has changed (script2), main would not work
+gcc -g -shared sh.o -o libsh.so -Wl,--version-script,script2
+
+# run main, expected error msg
+./main # ./main: ./libsh.so: version `LIBSH_1.0' not found (required by ./main)
